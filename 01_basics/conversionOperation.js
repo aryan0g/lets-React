fd74b78616ca7ce561c8a62cdf6e236e290d4d05
Aryan goel <138762494+aryan0g@users.noreply.github.com>
2025-04-29 13:15:48 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,9 @@
+"use strict"
+let number = 33
+let str = "aryan"
+let variable = {"firstName":"Aryan","lastName":"Goel"};
+console.table(variable);
+console.log(variable.firstName);
+let num = Number(str)
+console.log(num)
+//console.table(number,str)
